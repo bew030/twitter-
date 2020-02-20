44cf8ee95317d1379d8ac9a9a49be0847b95b3ee
Bernard Wong <bew030@its-wlh2015-16.ucsd.edu>
2020-02-19 22:00:42 -0800

    added like function

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -16,11 +16,22 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    // runs immediately once the view appears
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+    
     @IBAction func loginButton(_ sender: Any) {
         
         let url = "https://api.twitter.com/oauth/request_token"
         
         TwitterAPICaller.client?.login(url: url, success: {
+            
+            // sets login to true to keep the user logged in
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
+            
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print("could not log in")
